a22c2110b6666623921d88474b35b21b45ab58c7
EDY <963228468@qq.com>
2024-02-17 13:54:53 +0800

    1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @Author：xieHuaiCheng
+ * @Package：org.example
+ * @Project：Default (Template) Project
+ * @name：${NAME}
+ * @Date：2024/2/17 13:45
+ * @Filename：${NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
